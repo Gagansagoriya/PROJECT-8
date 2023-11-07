55a664ea5c7ee1ea6e50d322783e1951e70caaf9
Gagan Sagoriya <135191304+Gagansagoriya@users.noreply.github.com>
2023-11-07 15:17:57 +0530

    Add files via upload

---
+++ jokes2.js
@@ -0,0 +1,28 @@
+const jokes = document.querySelector('#joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+// async function genrateJokes (){
+    
+// }
+
+const genrateJokes = async () =>{
+    
+    try {
+
+        const setHeader = {
+            headers : {
+                Accept : "application/json"
+            }
+        }
+        const res = await fetch('https://icanhazdadjoke.com/',setHeader);
+        const data = await res.json();
+        jokes.innerHTML = data.joke;  
+    }catch(error) {
+        console.log(`the error is ${error}`);
+    }
+   
+}
+    
+
+jokeBtn.addEventListener('click' ,genrateJokes);
+genrateJokes();
